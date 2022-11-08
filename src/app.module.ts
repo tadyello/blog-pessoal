b80be21a84b36772ee0fb00012909d081d6e4e77
Tadyêllo Bastos <tadyello@hotmail.com>
2022-11-07 23:22:59 -0300

    Swagger/Deploy

--- src/app.module.ts
+++ src/app.module.ts
@@ -12,28 +12,28 @@ import { UsuarioModule } from './usuario/usuario.module';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: 'root',
-      database: 'db_blogpessoal',
-      entities: [Postagem, Tema, Usuario],
-      synchronize: true
-    }),
-
     // TypeOrmModule.forRoot({
-    //   type: 'postgres',
-    //   url: process.env.DATABASE_URL,
-    //   logging: false,
-    //   dropSchema: false,
-    //   ssl: {
-    //     rejectUnauthorized: false,
-    //   },
-    //   synchronize: true,
-    //   autoLoadEntities: true,
+    //   type: 'mysql',
+    //   host: 'localhost',
+    //   port: 3306,
+    //   username: 'root',
+    //   password: 'root',
+    //   database: 'db_blogpessoal',
+    //   entities: [Postagem, Tema, Usuario],
+    //   synchronize: true
     // }),
+
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      url: process.env.DATABASE_URL,
+      logging: false,
+      dropSchema: false,
+      ssl: {
+        rejectUnauthorized: false,
+      },
+      synchronize: true,
+      autoLoadEntities: true,
+    }),
     PostagemModule,
     TemaModule,
     UsuarioModule,
