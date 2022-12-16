2a0bddfbc0e0615965c99fbbea4730c0bf22c822
Tadyêllo Bastos <105318744+tadyello@users.noreply.github.com>
2022-12-16 16:20:04 -0300

    Update tema.service.ts

--- src/tema/service/tema.service.ts
+++ src/tema/service/tema.service.ts
@@ -38,10 +38,10 @@ export class TemaService {
         return tema
     }
 
-    async findByTema(tema: string): Promise<Tema[]> {
+    async findByTema(nome: string): Promise<Tema[]> {
         return await this.temaRepository.find({
             where: {
-                tema: ILike(`%${tema}%`)
+                nome: ILike(`%${nome}%`)
             },
             relations: {
                 postagem: true
