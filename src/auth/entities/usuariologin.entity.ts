33eaf9cc6df36848190aec07770b0a7658a94153
Tadyêllo Bastos <105318744+tadyello@users.noreply.github.com>
2022-12-19 16:27:59 -0300

    Update usuariologin.entity.ts

--- src/auth/entities/usuariologin.entity.ts
+++ src/auth/entities/usuariologin.entity.ts
@@ -3,9 +3,15 @@ import { ApiProperty } from "@nestjs/swagger/dist"
 export class UsuarioLogin {
 
     @ApiProperty()
+    id: number
+    
+    @ApiProperty()
     usuario: string
 
     @ApiProperty()
     senha: string
+    
+    @ApiProperty()
+    token: string
 
 }
